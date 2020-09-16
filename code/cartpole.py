125bb446af7aa75fa878aad264c42ce5458c32f7
Jay Gupta <jay002@e.ntu.edu.sg>
2020-09-16 16:54:34 +0800

    cartplole - random policy

--- code/cartpole.py
+++ code/cartpole.py
@@ -1,1 +1,36 @@
 # Code for Cartpole
+# Random Policy
+
+# Import all the libraries
+import gym
+import numpy as np
+from gym import wrappers
+
+# Load the environment
+env = gym.make('CartPole-v0')
+
+done = False
+count = 0
+
+# Record the environment
+env = wrappers.Monitor(env, 'recording', force=True)
+
+# Reset
+observation = env.reset()
+
+while not done:
+    # Render the environment on the screen
+    env.render()
+
+    count += 1
+
+    # Take a random action
+    action = env.action_space.sample()
+
+    # Observe the environment and get the reward
+    observation, reward, done, _ = env.step(action)
+
+# Close
+env.close()
+
+print('Game Duration: ', count, 'moves')
